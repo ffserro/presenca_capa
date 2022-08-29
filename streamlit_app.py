e544a1e824c1f5a14ee2a3534935207330bb1910
ffserro <ffserro@hotmail.com>
2022-08-29 00:21:44 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,6 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 from datetime import datetime
 st.set_page_config(page_title='Presença', page_icon='https://www.marinha.mil.br/sites/default/files/favicon-logomarca-mb.ico', layout="centered", menu_items=None)
 
-firebaseConfig = {
-  'apiKey': "AIzaSyA5ylnqAg64aMqPixVvh0JXABvI1tizeVk",
-  'authDomain': "prodesex-8e59f.firebaseapp.com",
-  'databaseURL': "https://prodesex-8e59f-default-rtdb.firebaseio.com",
-  'projectId': "prodesex-8e59f",
-  'storageBucket': "prodesex-8e59f.appspot.com",
-  'messagingSenderId': "191772777462",
-  'appId': "1:191772777462:web:cb327de1c21875275685d7",
-  'measurementId': "G-Z576SW3PKH"
-}
-
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 
