b420cb834cfab773aa10ef5ddb4475095257eea7
ffserro <ffserro@hotmail.com>
2022-08-29 00:16:26 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd 
-import pyrebase as pb
+import pyrebase
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 from datetime import datetime
 st.set_page_config(page_title='Presença', page_icon='https://www.marinha.mil.br/sites/default/files/favicon-logomarca-mb.ico', layout="centered", menu_items=None)
@@ -16,10 +16,12 @@ firebaseConfig = {
   'measurementId': "G-Z576SW3PKH"
 }
 
+firebase = pyrebase.initialize_app(firebaseConfig)
+db = firebase.database()
 
-#query = db.child('itens').get().val().values()
 
 df = pd.read_csv('./CAPA.csv')
+presentes = []
 
 #Infer basic colDefs from dataframe types
 gb = GridOptionsBuilder.from_dataframe(df)
@@ -50,7 +52,6 @@ grid_response = AgGrid(
     fit_columns_on_grid_load = True,
     data_return_mode='FILTERED', 
     update_mode='GRID_CHANGED',
-    allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
     theme='streamlit'    
     )
 
@@ -58,6 +59,15 @@ st.write(grid_response['selected_rows'])
 
 enviar = st.button('Presente!')
 if enviar:
+    for nome in grid_response['selected_rows']:
+        presentes.append(nome['NOME'])
+
+st.write(presentes)
+
+
+
+    '''
     ids = [i['id'] for i in grid_response['selected_rows']]
     for i in ([list(db.child('itens').order_by_child('id').equal_to(x).get().val().keys())[0] for x in ids]):
         db.child('itens').child(i).update({'situacao':'Em trânsito', 'data_envio':datetime.now().strftime("%d/%m/%Y")})
+    '''
