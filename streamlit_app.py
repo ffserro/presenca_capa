022dd2314d3112e99bc3aee91bfe05c1a550fcc5
ffserro <ffserro@hotmail.com>
2022-08-31 07:03:48 -0300

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,10 +69,9 @@ if len(df) != 0:
         st.experimental_rerun()
 else:
     st.title('Todos a bordo!')
-    def hidden():
-        final = pd.DataFrame()
-        for i in nomes.NOME:
-            hora = db.child('presenca').child(i).child(agora.strftime('%Y-%m-%d')).get().val()
-            final = pd.concat([final, pd.DataFrame({'Oficial Aluno':[i], 'Chegada':[hora]})], ignore_index=True)
-        final = final.style.hide_index()
-        st.write(final.to_html(), unsafe_allow_html=True)
+    final = pd.DataFrame()
+    for i in nomes.NOME:
+        hora = db.child('presenca').child(i).child(agora.strftime('%Y-%m-%d')).get().val()
+        final = pd.concat([final, pd.DataFrame({'Oficial Aluno':[i], 'Chegada':[hora]})], ignore_index=True)
+    final = final.sort_values('Chegada').style.hide_index()
+    st.write(final.to_html(), unsafe_allow_html=True)
